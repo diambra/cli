81fd5d716ee224fafbc2c1d8d922531c84adc6e3
Johannes 'fish' Ziemke <github@freigeist.org>
2022-05-20 13:17:54 +0200

    Replace pipesPath by port mappings
    This is to support the new gRPC mode

--- container/runner.go
+++ container/runner.go
@@ -1,6 +1,11 @@
 package container
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
 
 type BindMount struct {
 	HostPath      string
@@ -9,6 +14,70 @@ type BindMount struct {
 
 func NewBindMount(hostPath, containerPath string) *BindMount {
 	return &BindMount{hostPath, containerPath}
+}
+
+// e.g 80/tcp
+type Port string
+
+func (p *Port) split() (port int, proto string, err error) {
+	var (
+		parts   = strings.SplitN(string(*p), "/", 2)
+		portStr = ""
+	)
+	proto = "TCP"
+
+	if len(parts) == 2 {
+		portStr = parts[0]
+		proto = parts[1]
+	} else {
+		portStr = parts[0]
+	}
+	port, err = strconv.Atoi(portStr)
+	if err != nil {
+		return 0, "", fmt.Errorf("port '%s' invalid: %w", portStr, err)
+	}
+	return port, proto, nil
+}
+func (p *Port) Number() (int, error) {
+	port, _, err := p.split()
+	return port, err
+}
+
+/*
+func NewPort(port string) (*Port, error) {
+	var (
+		p       = strings.SplitN(port, "/", 2)
+		portStr = ""
+		proto   = "TCP"
+	)
+	if len(p) == 2 {
+		portStr = p[0]
+		proto = p[1]
+	} else {
+		portStr = port
+	}
+
+	portn, err := strconv.Atoi(portStr)
+	if err != nil {
+		return nil, fmt.Errorf("port '%s' invalid: %w", portStr, err)
+	}
+	return &Port{proto, portn}, nil
+}
+
+func (p *Port) String() string {
+	return fmt.Sprintf("%d/%s", p.Number, p.Protocol)
+}
+*/
+
+type Address struct {
+	Host string
+	Port Port
+}
+
+type PortMapping map[Port]Address
+
+func (pm *PortMapping) AddPortMapping(containerPort string, hostPort string, hostAddress string) {
+	(*pm)[Port(containerPort)] = Address{Host: hostAddress, Port: Port(hostPort)}
 }
 
 type Container struct {
@@ -20,10 +89,12 @@ type Container struct {
 	MemoryLimitBytes int64
 	CPULimitCores    float64
 	BindMounts       []*BindMount
+	PortMapping      *PortMapping
 }
 
 type ContainerStatus struct {
-	ID string
+	ID          string
+	PortMapping *PortMapping
 }
 
 type Runner interface {
--- diambra/diambra.go
+++ diambra/diambra.go
@@ -1,8 +1,9 @@
 package diambra
 
 import (
+	"fmt"
 	"io"
-	"strconv"
+	"strings"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -12,17 +13,18 @@ import (
 
 const (
 	DefaultEnvImage = "diambraengine:latest"
+	ContainerPort   = "50051/tcp"
 )
 
 type EnvConfig struct {
-	LockFPS bool
-	GUI     bool
-	Audio   bool
-	Scale   int
+	LockFPS    bool
+	GUI        bool
+	Audio      bool
+	Scale      int
+	AutoRemove bool
 
-	PipesPath string
-	RomsPath  string
-	CredPath  string
+	RomsPath string
+	CredPath string
 
 	User string
 
@@ -31,68 +33,87 @@ type EnvConfig struct {
 }
 
 type Env struct {
-	log.Logger
-	container.Runner
-	containers []*container.ContainerStatus
-	config     *EnvConfig
+	*container.ContainerStatus
+	container.Address
 }
 
-func NewEnv(logger log.Logger, config *EnvConfig) (*Env, error) {
-	runner, err := container.NewDockerRunner(logger)
+type Diambra struct {
+	log.Logger
+	container.Runner
+	Envs   []*Env
+	config *EnvConfig
+}
+
+func NewDiambra(logger log.Logger, config *EnvConfig) (*Diambra, error) {
+	runner, err := container.NewDockerRunner(logger, config.AutoRemove)
 	if err != nil {
 		return nil, err
 	}
-	return &Env{
-		Logger:     logger,
-		Runner:     runner,
-		containers: []*container.ContainerStatus{},
-		config:     config,
+	return &Diambra{
+		Logger: logger,
+		Runner: runner,
+		Envs:   []*Env{},
+		config: config,
 	}, nil
 }
 
-func (e *Env) Start() error {
+func (e *Diambra) EnvsString() (string, error) {
+	envs := make([]string, len(e.Envs))
+	for i, env := range e.Envs {
+		portn, err := env.Port.Number()
+		if err != nil {
+			return "", fmt.Errorf("invalid port %s: %w", env.Port, err)
+		}
+		envs[i] = fmt.Sprintf("127.0.0.1:%d", portn)
+	}
+	return strings.Join(envs, " "), nil
+}
+
+func (e *Diambra) Start() error {
 	for i := 0; i < e.config.Scale; i++ {
 		level.Debug(e.Logger).Log("msg", "creating env container", "envID", i)
 		cs, err := e.Runner.Start(newEnvContainer(e.config, i))
 		if err != nil {
 			return err
 		}
-		level.Debug(e.Logger).Log("msg", "started env container")
-		e.containers = append(e.containers, cs)
-		level.Debug(e.Logger).Log("msg", "appended container to list")
+		level.Debug(e.Logger).Log("msg", "started env container", "id", cs.ID)
+		env := &Env{
+			ContainerStatus: cs,
+			Address:         (*cs.PortMapping)[ContainerPort],
+		}
+		e.Envs = append(e.Envs, env)
 
-		level.Debug(e.Logger).Log("msg", "starting gofunc")
 		go func(id string) {
 			level.Debug(e.Logger).Log("msg", "in go func")
 			e.Runner.CopyLogs(id, e.config.Stdout, e.config.Stderr)
 			level.Debug(e.Logger).Log("msg", "end of go func")
-		}(e.containers[i].ID)
+		}(cs.ID)
 		level.Debug(e.Logger).Log("msg", "logs copying..")
 	}
-	level.Debug(e.Logger).Log("msg", "Start() end")
 	return nil
 }
 
 func newEnvContainer(config *EnvConfig, envID int) *container.Container {
-	envIDStr := strconv.Itoa(envID)
+	pm := &container.PortMapping{}
+	pm.AddPortMapping(ContainerPort, "0/tcp", "127.0.0.1")
+
 	return &container.Container{
-		Image:   DefaultEnvImage,
-		Command: []string{"--pipesPath", "/pipes", "--envId", envIDStr},
-		Env:     []string{"PIPES_PATH=/pipes", "ENV_ID=" + envIDStr},
-		User:    config.User,
+		Image: DefaultEnvImage,
+		//Command:     []string{"--pipesPath", "/pipes", "--envId", envIDStr},
+		User:        config.User,
+		PortMapping: pm,
 		BindMounts: []*container.BindMount{
-			container.NewBindMount(config.PipesPath, "/pipes"),
 			container.NewBindMount(config.CredPath, "/tmp/.diambraCred"),
 			container.NewBindMount(config.RomsPath, "/opt/diambraArena/roms"),
 		},
 	}
 }
 
-func (e *Env) Cleanup() error {
+func (e *Diambra) Cleanup() error {
 	var rerr error
-	for _, c := range e.containers {
-		level.Debug(e.Logger).Log("msg", "stopping container", "id", c.ID)
-		if err := e.Runner.Stop(c.ID); err != nil {
+	for _, env := range e.Envs {
+		level.Debug(e.Logger).Log("msg", "stopping container", "id", env.ContainerStatus.ID)
+		if err := e.Runner.Stop(env.ContainerStatus.ID); err != nil {
 			rerr = err
 			level.Warn(e.Logger).Log("msg", "couldn't stop container", "err", err.Error())
 		}
